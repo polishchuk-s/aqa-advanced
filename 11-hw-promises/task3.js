028d37d38ee18bdadea4df4dd859ee526c7af2fa
Johnny Galt <84209055+polishchuk-s@users.noreply.github.com>
2025-04-01 18:09:48 +0200

    adding 11-hw-promises

---
+++ 11-hw-promises/task3.js
@@ -0,0 +1,37 @@
+async function fetchTodo () { 
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('Error:', error);
+        throw error;
+    }
+};
+
+async function fetchUser () { 
+    try {
+        const response2 = await fetch('https://jsonplaceholder.typicode.com/users/1');
+        const data2 = await response2.json();
+        return data2;
+    } catch (error) {
+        console.log('Error:', error);
+        throw error;
+    }
+};
+
+Promise.all([fetchTodo(), fetchUser()])
+.then((values) => {
+    console.log(`Promise.all values are: `);
+    console.log(values);
+})
+.catch(error => {
+    console.error(error);
+});
+
+Promise.race([fetchTodo(), fetchUser()])
+.then((values) => {
+    console.log(`Promise.race values are: `);
+    console.log(values);
+});
+
